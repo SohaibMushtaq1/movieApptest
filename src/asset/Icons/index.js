1e851d59c0a9821343c658918b27d19624190eab
Sohaib_Mushtaq621 <70438253+SohaibMushtaq1@users.noreply.github.com>
2023-12-24 18:51:11 +0500

    first commit

---
+++ src/asset/Icons/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+const ICON = {
+  DASGBOARD: require('./DashBoard.png'),
+  MORE: require('./More.png'),
+  MEDIA_LIBRARY: require('./MediaLibrary.png'),
+  WATCH: require('./Watch.png'),
+};
+
+export {ICON}
---
+++ src/screens/index.js
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Main = () => {
+    const data =[
+        {
+            id:1,
+            title:"DashBorad"
+        },
+        {
+            id:2,
+            title:"Watch"
+        },
+        {
+            id:3,
+            title:"Media Library"
+        },
+        {
+            id:4,
+            title:"More"
+        },
+    ]
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+export default Main
+
+const styles = StyleSheet.create({});
